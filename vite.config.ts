695dbff1d3fd3c15c43110c7fa4aa350f7713dcb
Seb <sebtoombs@gmail.com>
2023-01-09 10:48:17 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          chakra: ["@chakra-ui/react", "@emotion/react", "@emotion/styled"],
+          konva: ["konva"],
+          dnd: ["react-dnd", "react-dnd-html5-backend"],
+        },
+      },
+    },
+  },
+});
